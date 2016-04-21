32c96e04f11ae1e75b4d2ac04a3b5e29e5d5cdc3
Vlad Vanca <vanca.vlad@gmail.com>
2016-04-21 15:58:02 +0300

    Update initializer for Ember 2.x

--- addon/initializers/ember-social-services.js
+++ addon/initializers/ember-social-services.js
@@ -1,22 +1,31 @@
+import FacebookApiClient from '../services/facebook-api-client';
+import InstagramApiClient from '../services/instagram-api-client';
+import LinkedinApiClient from '../services/linkedin-api-client';
+import TwitterApiClient from '../services/twitter-api-client';
+
 export default {
   name: 'ember-social-services',
 
   initialize: function(){
     let application = arguments[1] || arguments[0];
 
+    application.register('service:facebook-api-client', FacebookApiClient);
     var facebookPluginComponents = ['facepile', 'like', 'share'];
 
     facebookPluginComponents.forEach(function(plugin) {
       application.inject('component:facebook-' + plugin, 'socialApiClient', 'service:facebook-api-client');
     });
 
+    application.register('service:twitter-api-client', TwitterApiClient);
     var twitterComponents = ['share', 'follow', 'card'];
     twitterComponents.forEach(function(buttonType) {
       application.inject('component:twitter-' + buttonType, 'socialApiClient', 'service:twitter-api-client');
     });
 
+    application.register('service:linkedin-api-client', LinkedinApiClient);
     application.inject('component:linkedin-share', 'socialApiClient', 'service:linkedin-api-client');
 
+    application.register('service:instagram-api-client', InstagramApiClient);
     application.inject('component:instagram-follow', 'socialApiClient', 'service:instagram-api-client');
   }
 };
